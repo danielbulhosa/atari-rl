8222c593b443483ed68790efbc92d5af061fd2f1
Daniel Bulhosa Solorzano <dbulhosa@alum.mit.edu>
2020-05-12 23:17:36 -0700

    Make GoogLeNet match paper spec as closely as possible

--- models/googlenet/v00/rundef.py
+++ models/googlenet/v00/rundef.py
@@ -9,6 +9,7 @@ import keras.regularizers as reg
 import keras.initializers as init
 import shared.custom_layers.inception_module as custom
 import shared.definitions.paths as paths
+from shared.generators.augmentation_list import AugmentationList
 import albumentations
 import os
 from os import path
@@ -18,14 +19,15 @@ Model Definition
 """
 version = 0
 
-# FIXME: Initialization and regularization not clearly specified in paper
+# Can't find initialization or regularization of weights in paper. Using same as ResNet.
 init_reg = {
     'kernel_initializer': init.he_uniform(),
     'bias_initializer': init.Zeros(),
-    'kernel_regularizer': reg.l2(0.0005),
-    'bias_regularizer': reg.l2(0.0005)
+    'kernel_regularizer': reg.l2(0.0001),
+    'bias_regularizer': reg.l2(0.0001)
 }
 
+# Note we're not adding the auxiliary classifiers, just to keep it simple.
 input = lyr.Input((224, 224, 3,))
 lyrout1 = lyr.Conv2D(64, 7, strides=2, padding='same', activation='relu', **init_reg)(input)
 lyrout2 = lyr.MaxPool2D(3, 2, 'same')(lyrout1)
@@ -57,10 +59,11 @@ Optimizer, Loss, & Metrics
 Using zero built in decay and relying exclusively on the heuristic
 used in the original paper.
 """
-# FIXME: Is this a good starting optimizer?
-optimizer = opt.Adam(0.0001)
-# This line below would match the paper exactly but was MUCH slower than using Adam on 2 class example
-# optimizer = opt.SGD(learning_rate=0.01, momentum=0.9, decay=0.0) # Note decay refers to learning rate
+optimizer = opt.Adam(0.001)
+# This line below is what was used in the original ResNet paper. Adam is the recommended approach now though.
+# We use this for GoogLeNet because its paper only specifies that they used momentum=0.9 but not what the initial
+# learning rate was.
+# optimizer = opt.SGD(learning_rate=0.1, momentum=0.9, decay=0.0) # Note decay refers to learning rate
 
 top_1_acc = met.categorical_accuracy
 
@@ -81,26 +84,21 @@ model.compile(optimizer=optimizer,
 """
 Epochs & Batch Sizes
 """
-# FIXME: Number of epochs for this model?
+# Can't find references to this in the paper. Using what we used for ResNet.
+# No references for number of epochs to just leaving it at 90.
 num_epochs = 90
-train_batch_size = 128
-val_batch_size = 128
-test_batch_size = 13
+train_batch_size = 256
+val_batch_size = 256
+test_batch_size = 26
 
 """
 Callback Params
 """
 
-# FIXME - start with defaults for this model?
-scheduler_params = {'factor': 0.1,
-                    'monitor': 'val_loss',
-                    'verbose': 1,
-                    'mode': 'auto',
-                    'patience': 5,
-                    'min_lr': 10**(-8),
-                    'min_delta': 0.0001}
+scheduler_params = {'schedule': lambda epoch, lr: 0.96 * lr if (epoch + 1) % 4 == 0 else lr,
+                    'verbose': 1}
 
-scheduler = call.ReduceLROnPlateau(**scheduler_params)
+scheduler = call.LearningRateScheduler(**scheduler_params)
 
 if not path.isdir('checkpoints'):
     os.mkdir('checkpoints')
@@ -122,12 +120,20 @@ checkpointer_params = {'filepath': paths.models + 'googlenet/v{:02d}/checkpoints
 Augmentation Parameters
 """
 
-# FIXME: Which augmentations to start with for GoogLeNet?
-aug_list = [albumentations.ShiftScaleRotate(shift_limit=0.1, scale_limit=0.2, rotate_limit=25),
-            albumentations.HorizontalFlip()]
+# These are the augmentations from the GoogLeNet paper, which come from: https://arxiv.org/pdf/1312.5402.pdf
+# We do not know how the perturbation magnitude in paper maps to albumentations so just used library defaults
 
-# FIXME: Do we want this augmentation for this model?
-shift_scale = 1
+# Furthermore, the GoogLeNet paper recommends "sampling of various sized patches of the image whose size is distributed
+# evenly between 8% and 100% of the image area with aspect ratio constrained to the interval [3/4, 4/3]."
+# We upscale the image by 3.5 so that the 224x224 sampled patch has 8% area of original image. We scale
+# uniformly though.
+aug_list = AugmentationList(albumentations.RandomBrightnessContrast(p=1),
+                            albumentations.RGBShift(p=1),
+                            albumentations.RandomScale(scale_limit=(1, 3.5), p=1),
+                            albumentations.HorizontalFlip(),
+                            shuffle=True)
+
+shift_scale = 0.1
 
 
 """

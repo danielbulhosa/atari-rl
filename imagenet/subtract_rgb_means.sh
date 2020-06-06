5d42095e7cd1114f9fff7e0334044d03fd5651d3
Daniel Bulhosa Solorzano <dbulhosa@alum.mit.edu>
2020-06-05 23:56:52 -0700

    Removing files not needed for RL projects

--- imagenet/subtract_rgb_means.sh
+++
@@ -1,8 +0,0 @@
-# See: https://www.imagemagick.org/Usage/transform/#evaluate
-# Note that RGB values are normalized to 0-1 so $X = (# pixels)/255
-
-find . -name "*.JPEG" | xargs -I {}  convert {} \
--channel R -evaluate subtract $1 \
--channel G -evaluate subtract $2 \
--channel B -evaluate subtract $3 \
-{}

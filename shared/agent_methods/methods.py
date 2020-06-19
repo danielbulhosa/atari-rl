82640b7c3773859521a0f704dfe4fb92c8d8c16f
Daniel Bulhosa Solorzano <dbulhosa@alum.mit.edu>
2020-06-18 23:59:49 -0700

    Fix action selection from Q model
    The model predicting Q values expects an array of states,
    and the predict method returns a 2D array of action values
    by the number of samples passed to the predict method.
    
    Although we wrote this method to be able to predict multiple
    output actions for multiple input states, we only ever really
    need to pass one state. For that reason we limited the states
    passed to one, and then assumming this to be the case we fixed
    the parsing of the action out of the predicted Q values.

--- shared/agent_methods/methods.py
+++ shared/agent_methods/methods.py
@@ -10,19 +10,27 @@ def evaluate_state(model, states):
 
 
 # FIXME - replace equivalent code in environment and agent evaluation classes
-def get_action(model, environment, states, epsilon, iteration):
+def get_action(model, environment, state, epsilon, iteration):
     current_epsilon = epsilon(iteration)  # Note epsilon is a method
 
     is_greedy = np.random.binomial(1, 1 - current_epsilon)
 
     if is_greedy:
-        actions = [0 for state in states]  # We don't actually care about indexing Q_0
+        actions = [0 for state in state]  # We don't actually care about indexing Q_0
+        assert len(state) == 1, "Only one state should be passed to this method"
 
-        Q_a = model.predict([np.array(states), np.array(actions)])[0]
-        max_actions = np.argwhere(Q_a == np.amax(Q_a)).flatten()
+        # Note in practice
+        Q_a = model.predict([np.array(state), np.array(actions)])[0]
+        # Note we need to reshape because output is 2D array.
+        # Each 2D Array entry is the 2D index of a max. Since
+        # We only pass one state max_states should have length
+        # between 1 and # of possible actions.
+        max_actions = np.argwhere(Q_a == np.amax(Q_a, axis=1).reshape(-1, 1))[:, 1]
+
         action = np.random.choice(max_actions)
 
     else:
         action = environment.action_space.sample()
 
     return action
+

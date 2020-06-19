7dfa917fe9dacebab25796215578cfef388e2cac
Daniel Bulhosa Solorzano <dbulhosa@alum.mit.edu>
2020-06-19 00:01:48 -0700

    Changes to evaluation callback
    - Add call on training start that evaluates performance of
      random policy and policy derived from initial model.
    - Add calculation of metric measuring average number of
      timesteps per episode. Log that metric to Tensorboard.
    - Move simulation logic into its own method so it can
      be shared by the on_epoch_end and on_train_start methods.
    
    After doing this we found that the initial model (which is
    initialized with random weights) performs worse than the
    random policy, unless the initial weights are constant. This
    suggests to us there is something wrong with the model since
    the model shouldn't be biased towards a single at the beginning
    if the initial weights are random. We are investigating this
    further.

--- shared/custom_callbacks/evaluate_agent.py
+++ shared/custom_callbacks/evaluate_agent.py
@@ -18,10 +18,10 @@ class EvaluateAgentCallback(call.Callback):
         self.num_init_samples = num_init_samples
         self.step_number = 0
 
-    def on_epoch_end(self, epoch, logs=None):
+    def simulate_episodes(self, action_getter):
 
         total_rewards = np.empty(self.num_episodes)
-        init_states = []
+        episode_lenghts = np.empty(self.num_episodes)
 
         for episode in range(self.num_episodes):
             iteration = 0
@@ -32,14 +32,33 @@ class EvaluateAgentCallback(call.Callback):
 
             while not done:
                 iteration += 1
-                action = self.get_action(observation, iteration)
+                action = action_getter(observation, iteration)
                 observation, reward, done, info = self.environment.step(action)
                 total_reward += discount_factor * reward
                 discount_factor *= self.gamma
 
             total_rewards[episode] = total_reward
+            episode_lenghts[episode] = iteration
 
         average_reward = np.mean(total_rewards)
+        average_episode_length = np.mean(episode_lenghts)
+
+        return average_reward, average_episode_length
+
+    def on_train_begin(self, logs=None):
+        average_reward, average_episode_length = self.simulate_episodes(self.get_action)
+        random_reward, random_episode_length = self.simulate_episodes(self.get_random_action)
+
+        print("\nInitial valuation. " +
+              "\nAverage reward of initial policy over {} episodes: {}".format(self.num_episodes, average_reward) +
+              "\nAverage reward of random policy over {} episodes: {}".format(self.num_episodes, random_reward) +
+              "\nAverage episode length of initial policy over {} episodes: {}".format(self.num_episodes, average_episode_length) +
+              "\nAverage episode length of random policy over {} episodes: {}".format(self.num_episodes, random_episode_length))
+
+    def on_epoch_end(self, epoch, logs=None):
+
+        average_reward, average_episode_length = self.simulate_episodes(self.get_action)
+        init_states = []
 
         for num in range(self.num_init_samples):
             observation = self.environment.reset()
@@ -51,6 +70,7 @@ class EvaluateAgentCallback(call.Callback):
         # Code borrowed from: https://chadrick-kwag.net/how-to-manually-write-to-tensorboard-from-tf-keras-callback-useful-trick-when-writing-a-handful-of-validation-metrics-at-once/
         items_to_write = {
             "average_reward": average_reward,
+            "average_episode_length": average_episode_length,
             "average_initial_state_value": average_init_value
         }
         writer = self.tb_callback.writer
@@ -65,10 +85,14 @@ class EvaluateAgentCallback(call.Callback):
 
         print("\nEvaluation complete. " +
               "\nAverage reward over {} episodes: {}".format(self.num_episodes, average_reward) +
+              "\nAverage episode length over {} episodes: {}".format(self.num_episodes, average_episode_length) +
               "\nAverage expected value across {} initial states: {}".format(self.num_init_samples, average_init_value))
 
     def get_action(self, state, iteration):
         return agmeth.get_action(self.model, self.environment, [state], self.epsilon, iteration)
+
+    def get_random_action(self, state, iteration):
+        return self.environment.action_space.sample()
 
     def evaluate_state(self, states):
         return agmeth.evaluate_state(self.model, states)

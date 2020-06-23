00ade117a6eadb1d1c34a8502f9a999406895d27
Daniel Bulhosa Solorzano <dbulhosa@alum.mit.edu>
2020-06-23 15:10:26 -0700

    Loop tests by episodes, print rewards, add video record

--- shared/runners/test.py
+++ shared/runners/test.py
@@ -2,40 +2,59 @@ import sys
 import importlib
 import numpy as np
 import shared.agent_methods.methods as agmeth
+from gym import wrappers
+
 agent_path = sys.argv[1]
 print(agent_path)
 rundef = importlib.import_module(agent_path)  # Run definition
 
 """Model Loading (If Applicable)"""
 checkpoint_dir = rundef.loading_params['test_checkpoint_dir']
-model_file = rundef.loading_params['test_model_file']
+model_file =  rundef.loading_params['test_model_file']
+model_dir = rundef.loading_params['test_model_dir']
 
 assert model_file is not None and checkpoint_dir is not None, "Need to define model to test"
-print("Loading Model")
-rundef.model.load_weights(checkpoint_dir + model_file)
 
 env = rundef.environment
 epsilon = lambda iter: 0.00
+gamma = 1
+num_episodes = 100
 observation = env.reset()
 render = False
-actions = []
+increment_model = False
+save_video = False
+checkpoint_start = 1
 
-for iter in range(10000):
-  if render:
-    env.render()
-  action = agmeth.get_action(rundef.model, env, [observation], epsilon, iter)
-  actions.append(action)
-  observation, reward, done, info = env.step(action)
+# By default only perfect cube recordings get saved. Callable passed here changes that.
+env = env if not save_video else wrappers.Monitor(env, model_dir + '/videos/', video_callable=lambda episode_id: True)
 
-  if done:
+total_reward = 0
+rewards = []
+first_episode = True
+
+for episode in range(num_episodes):
+    done = False
+    step = 0
+    if increment_model or first_episode:
+        print("\nCheckpoint #{}".format(checkpoint_start))
+        rundef.model.load_weights(checkpoint_dir + model_file.format(epoch=checkpoint_start))
+        checkpoint_start += 1
+        first_episode = False
+
     observation = env.reset()
+
+    while not done:
+        if render:
+          env.render()
+
+        action = agmeth.get_action(rundef.model, env, [observation], epsilon, iter)
+        observation, reward, done, info = env.step(action)
+        total_reward += gamma**step * reward
+        step += 1
+
+    print("Total reward: {}".format(total_reward))
+    rewards.append(total_reward)
+    total_reward = 0
+
+print("Average Reward Over All Episodes: {}".format(np.mean(np.array(rewards))))
 env.close()
-
-if __name__ == '__main__':
-  action_groupby = {action: 0 for action in range(env.action_space.n)}
-
-  for action in actions:
-      action_groupby[action] += 1
-
-  print(action_groupby)
-

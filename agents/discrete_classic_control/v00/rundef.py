a9e20420a83aacc3ea87adf0f29134c68ea7fbc8
Daniel Bulhosa Solorzano <dbulhosa@alum.mit.edu>
2020-06-22 23:40:47 -0700

    Critical fix: Q_a output tensor shape fix (and other changes)
    We were running into a major issue where the gradient was not
    updating correctly. The value function would uniformly increase
    towards 9.35, which is the value of the constant policy. It would
    then stay there, and the losss would be constant and about 8. We
    found that the Q function estimator was not fitting the "target"
    value function at terminal states, and this was the source of the
    error.
    
    Because of this we suspected there was an error with the gradients.
    We wrote a debugging function to print out the gradients and losses
    for the network. We found that the loss had the wrong shape, and
    the resulting loss and gradients were wrong. We found that the shape
    problem was because the Q_a output (output 2 of the network) had the
    wrong shape, so we fixed the shape of it in the model and also
    in the generator. The changes to the genrator are in a previous
    commit, and the changes to the model can be found under the
    `get_Q_a` method in this file.
    
    This solved the problem and our simple DQN for the cartpole problem
    works now!!! Finally!!! :,)
    
    Other changes we did along the way:
    
    - Add aforementioned debugging functions to main method of this
      file.
    - Initialize weights with random Gaussian distribution to avoid
      symmetry problems with weight values and gradient.
    - Change model to use simple one that was found to work by
      someoene else (https://towardsdatascience.com/cartpole-introduction-to-reinforcement-learning-ed0eb5b58288#f94f)
      We wanted to make sure we had a simple but powerful enough
      model that we knew should work.
    - Change optimizer to RMSProp, which is what was used in
      Atari paper. We thought this made sense since Adam is
      highly adaptive but adaptiveness and momentum may not
      make sense when the target model is being updated often.
    - Make training policy more exploratory, increasing
      frequency of exploratory action from 0.05 to 0.1.
    - Add `replay_buffer_min` parameter which controls how
      many iterations are used to prepopulate the replay
      memory with the random policy before value function
      learning starts.
    - Other changes to hyperparameters to improve learning
      stability and speed, and model clarity:
        - Target frequency written in terms of minibatch
          updates to align more clearly with minibatches.
        - Eliminate action repeat, since episodes are short
          anyways so changing actions at each step is important.
        - Increase replay buffer size for stability, write in
          terms of minibatch size.
        - Increase number of episodes and initial states sampled
          to estimate model performance, to reduce variance of
          performance estimates.
    
    - Add parameters that define which model checkpoint is used
      in `runners/test.py` for testing the model.
    
    This commit marks the first, truly working version of this
    repo!!! Yay!!!

--- agents/discrete_classic_control/v00/rundef.py
+++ agents/discrete_classic_control/v00/rundef.py
@@ -3,10 +3,12 @@ import keras.layers as lyr
 import keras.optimizers as opt
 import keras.metrics as met
 import keras.losses as losses
+import keras.initializers as init
 import shared.definitions.paths as paths
 import os
 from os import path
-from shared.generators.ennvironment_sequence import EnvironmentSequence
+from shared.generators.environment_sequence import EnvironmentSequence
+import shared.debugging.functions as debug
 import gym
 import copy
 
@@ -20,24 +22,27 @@ steps_per_epoch = None
 
 def get_Q_a(tensors):
     import tensorflow as tf  # FIXME - janky, need a better solution
-    return tf.gather_nd(tensors[0], tensors[1], batch_dims=1)
+    return tf.reshape(tf.gather_nd(tensors[0], tensors[1], batch_dims=1), (-1, 1))
 
 
 def Q_a_shape(input_shape):
     return input_shape[1]
 
 
+kernel_init = init.RandomUniform(-0.1, 0.1)
+
 input_size = 4
 output_size = 2  # Same as number of actions
 state_input = lyr.Input((input_size, ))
 action_input = lyr.Input((1, ), dtype='int32')
-intermediate1 = lyr.Dense(100, activation='relu')(state_input)
-value_out = lyr.Dense(output_size, activation='relu')(intermediate1)
+intermediate1 = lyr.Dense(24, activation='relu', kernel_initializer=kernel_init)(state_input)
+intermediate2 = lyr.Dense(24, activation='relu', kernel_initializer=kernel_init)(intermediate1)
+value_out = lyr.Dense(output_size, activation='linear', kernel_initializer=kernel_init)(intermediate2)
 # Note we use the action to index for the value used for the loss. It is NOT used as a feature for training,
 action_out = lyr.Lambda(get_Q_a, Q_a_shape)([value_out, action_input])
 model = mod.Model(inputs=[state_input, action_input], outputs=[value_out, action_out])
 
-optimizer = opt.Adam(0.001)  # FIXME - do we want to change our optimizer?
+optimizer = opt.RMSprop(0.001)  # FIXME - do we want to change our optimizer?
 
 
 model.compile(optimizer=optimizer,
@@ -53,17 +58,18 @@ Epochs & Batch Sizes
 # Note that we do not need a validation generator hence not val batch size.
 num_epochs = 90
 environment = gym.make("CartPole-v0")
-train_exploration_schedule = (lambda iteration: 0.05)
+train_exploration_schedule = (lambda iteration: 0.1)
 eval_exploration_schedule = (lambda iteration: 0.05)
 grad_update_frequency = 16
 train_batch_size = 32
-target_update_frequency = 10000
-action_repeat = 4
+target_update_frequency = 16 * 1000  # Multiplying by grad update freq lets us interpret the second number as epochs
+action_repeat = 1  # The average episode length at the beginning is 9 steps, 4 repetitions would be half the episode...
 gamma = 0.99
 epoch_length = 10000
-replay_buffer_size = 1000
-eval_episodes = 30
-eval_num_samples = 1000
+replay_buffer_size = 16 * 1000  # Multiplying by grad update freq lets us interpret the second number as epochs
+replay_buffer_min = 10000
+eval_episodes = 1000
+eval_num_samples = 10000
 
 """
 Callback Params
@@ -71,8 +77,6 @@ Callback Params
 
 # FIXME - Need to use learning rate scheduler used in different papers?
 scheduler = None
-
-# FIXME - Create callback to calculate agent performance?
 
 log_dir = paths.agents + 'discrete_classic_control/v{:02d}/logs'.format(version)
 checkpoint_dir = paths.agents + 'discrete_classic_control/v{:02d}/checkpoints'.format(version)
@@ -110,7 +114,10 @@ Loading Params
 # If model_file is not None and checkpoint_dir is not None then loading happens, else not
 loading_params = {'checkpoint_dir': None,
                   'model_file': None,
+                  'test_checkpoint_dir': checkpoint_dir,
+                  'test_model_file': None,  # Weight file used for testing performance
                   'epoch_start': None}
+
 
 train_gen = EnvironmentSequence(model,
                                 source_type='value',
@@ -122,11 +129,10 @@ train_gen = EnvironmentSequence(model,
                                 action_repeat=action_repeat,
                                 gamma=gamma,
                                 epoch_length=epoch_length,
-                                replay_buffer_size=replay_buffer_size)
+                                replay_buffer_size=replay_buffer_size,
+                                replay_buffer_min=replay_buffer_min)
 
 if __name__ == '__main__':
-    print(model.summary())
-    print(gym.make('CartPole-v0').reset())
-    for epoch in range(epoch_length):
-        print("Generator Test: Epoch {}".format(epoch))
-        train_gen[epoch]
+    model.summary()
+    debug.sample_weights_and_gradients(model, train_gen)
+    debug.test_generator(train_gen, epoch_length)

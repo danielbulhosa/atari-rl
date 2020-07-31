7f6704e979ce7e9c6202a14a7f9761db73afc50f
Daniel Bulhosa Solorzano <dbulhosa@alum.mit.edu>
2020-07-31 02:55:53 -0700

    Modify evaluation callback to use sequence class for simulation
    A major bug with the evaluation callback when used for the Atari
    environment is that the callback did not contain the methods for
    generating features and actions. In order to correct this we have
    modified the custom callback class so that:
    
    - Instad of passing the environment, the gamma value, and the
      exploration schedule, we just pass a method that constructs
      an instance of the appropriate SynchronousSequence subclass
      for the problem.
    
    - Each time simulations are required for evaluating the performance
      of an agent, an instance of the SynchronousSequence subclass is
      created and passed to the callback class's `simulate_episodes`
      method. The instantiation of sequence classes can be seen in the
      `on_train_begin` and `on_epoch_end` methods.
    
    - The `simulate_episodes` method utilizes the `simulate_single`,
      state getter methods, and internal iteration and episode tracker
      variables contained in the SynchronousSequence class. This way,
      the simulation logic of the callback has access to the logic for
      computing complex features and resulting actions contained in the
      sequence class.
    
    - Given that the sequence class provides the complete logic for
      getting states we removed the methods for getting actions from
      the custom callback class. The sequence class takes care of this
      now.
    
    We also added missing documentation for the callback class constructor
    parameter `num_max_iter`.

--- shared/custom_callbacks/evaluate_agent.py
+++ shared/custom_callbacks/evaluate_agent.py
@@ -6,8 +6,7 @@ import shared.agent_methods.methods as agmeth
 
 class EvaluateAgentCallback(call.Callback):
 
-    def __init__(self, environment, gamma, tb_callback,
-                 epsilon=(lambda iteration: 0.05), num_episodes=30,
+    def __init__(self, sequence_constructor, tb_callback, num_episodes=30,
                  num_init_samples=1000, num_max_iter=np.inf):
 
         """
@@ -25,26 +24,21 @@ class EvaluateAgentCallback(call.Callback):
         as it was found to be an estimator of model performance with
         less variance.
 
-        :param environment: The environment the agent (model) is
-               being trained on.
-        :param gamma: The discount rate for rewards. Used for
-               calculating total episode rewards.
+        :param sequence_constructor: Constructor for the sequence class
+               used to simulate the agent and the environment.
         :param tb_callback: The Tensorboard callback. We use this
                callback to write our evaluation metrics to the
                Tensorboard.
-        :param epsilon: A function taking in the current iteration
-               and returning the epsilon used to choose between
-               exploration and exploitation.
         :param num_episodes: The number of episodes to evaluate the
                agent on.
         :param num_init_samples: The number of initial states on which
                to sample the policy value on.
+        :param num_max_iter: The number of maximum iterations to simulate
+               for the purpose of evaluation.
         """
 
-        self.environment = environment
-        self.gamma = gamma
+        self.sequence_constructor = sequence_constructor
         self.tb_callback = tb_callback
-        self.epsilon = epsilon
         self.num_episodes = num_episodes
         self.num_init_samples = num_init_samples
         self.num_max_iter = num_max_iter
@@ -53,7 +47,7 @@ class EvaluateAgentCallback(call.Callback):
         assert num_episodes is not np.inf or num_max_iter is not np.inf, \
             "`num_episodes` and `num_max_iter` cannot both be equal to `np.inf`"
 
-    def simulate_episodes(self, action_getter):
+    def simulate_episodes(self, sequence):
         """
         Code for simulating an episode. Used in evaluation
         callback for simulating episodes with agent and
@@ -66,44 +60,46 @@ class EvaluateAgentCallback(call.Callback):
 
         total_rewards = []
         episode_lenghts = []
-        episode = 0
 
-        while episode < self.num_episodes:
-            iteration = 0
+        while sequence.episode <= self.num_episodes:
             discount_factor = 1
             total_reward = 0
-            observation = self.environment.reset()
-            done = False
+            done = sequence.get_latest_done()
+            assert not done, "Initial done of episode should always be `None` or `False`"
+            iteration_start = sequence.iteration
 
-            while not done and iteration <= self.num_max_iter:
-                iteration += 1
-                action = action_getter(observation, iteration)
-                observation, reward, done, info = self.environment.step(action)
+            while not done and sequence.iteration <= self.num_max_iter:
+                sequence.simulate_single()
+                reward = sequence.get_latest_reward()
+                done = sequence.get_latest_done()
                 total_reward += discount_factor * reward
-                discount_factor *= self.gamma
+                discount_factor *= sequence.gamma
 
             # If episode was not completed and max iterations reached then do not count the episode and break
-            if not done and iteration > self.num_max_iter:
+            if not done and sequence.iteration > self.num_max_iter:
                 break
             # Otherwise count the episode and see if we reached the max iterations yet, loop or exit accordingly
             else:
                 total_rewards.append(total_reward)
-                episode_lenghts.append(iteration)
+                episode_lenghts.append(sequence.iteration - iteration_start)
+                # Simulate one more step to restart environment
+                sequence.simulate_single()
 
-                if iteration > self.num_max_iter:
+                if sequence.iteration > self.num_max_iter:
                     break
-
-            episode += 1
 
         average_reward = np.mean(np.array(total_rewards))
         average_episode_length = np.mean(np.array(episode_lenghts))
-        num_episodes = episode
+        num_episodes = sequence.episode
 
         return average_reward, average_episode_length, num_episodes
 
     def on_train_begin(self, logs=None):
-        average_reward, average_episode_length, average_num_episodes = self.simulate_episodes(self.get_action)
-        random_reward, random_episode_length, random_num_episodes = self.simulate_episodes(self.get_random_action)
+        # Need a new sequence each time to reset instance state
+        policy_sequence = self.sequence_constructor()
+        average_reward, average_episode_length, average_num_episodes = self.simulate_episodes(policy_sequence)
+        random_sequence = self.sequence_constructor()
+        random_reward, random_episode_length, random_num_episodes = self.simulate_episodes(random_sequence)
 
         print("\nInitial valuation. " +
               "\nAverage reward of initial policy over {} episodes: {}".format(self.num_episodes, average_reward) +
@@ -114,12 +110,12 @@ class EvaluateAgentCallback(call.Callback):
               "\nNumber of episodes for random policy over {} episodes: {}".format(random_num_episodes, self.num_max_iter))
 
     def on_epoch_end(self, epoch, logs=None):
-
-        average_reward, average_episode_length, num_episodes = self.simulate_episodes(self.get_action)
+        policy_sequence = self.sequence_constructor()
+        average_reward, average_episode_length, num_episodes = self.simulate_episodes(policy_sequence)
         init_states = []
 
         for num in range(self.num_init_samples):
-            observation = self.environment.reset()
+            observation = policy_sequence.environment.reset()
             init_states.append(observation)
 
         init_sample_values = self.evaluate_state(np.array(init_states))
@@ -147,12 +143,6 @@ class EvaluateAgentCallback(call.Callback):
               "\nAverage episode length over {} episodes: {}".format(self.num_episodes, average_episode_length) +
               "\nNumber of episodes over {} max iterations".format(num_episodes, self.num_max_iter) +
               "\nAverage expected value across {} initial states: {}".format(self.num_init_samples, average_init_value))
-
-    def get_action(self, state, iteration):
-        return agmeth.get_action(self.model, self.environment, [state], self.epsilon, iteration)
-
-    def get_random_action(self, state, iteration):
-        return self.environment.action_space.sample()
 
     def evaluate_state(self, states):
         return agmeth.evaluate_state(self.model, states)

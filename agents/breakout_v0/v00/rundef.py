eaadf383150d2302565412c98597b3f0f4591308
Daniel Bulhosa Solorzano <dbulhosa@alum.mit.edu>
2020-08-02 03:54:52 -0700

    Remove action repeat parameter from agents, unneeded

--- agents/breakout_v0/v00/rundef.py
+++ agents/breakout_v0/v00/rundef.py
@@ -40,10 +40,10 @@ init_params = {'kernel_initializer': init.glorot_uniform()}
 
 # Note we don't need to set repeat_action_probability (ALE level) because repeat now handled at Gym package level
 # using `frameskip` parameter. See https://github.com/openai/gym/blob/master/gym/envs/atari/atari_env.py
-builtin_action_repeat = (3, 5)
-builtin_action_repeat_avg = builtin_action_repeat if isinstance(builtin_action_repeat, int) \
-                            else (builtin_action_repeat[1] - builtin_action_repeat[0])/2 + builtin_action_repeat[0]
-environment = gym.make("Breakout-v0", frameskip=builtin_action_repeat)
+action_repeat = 4
+action_repeat_avg = action_repeat if isinstance(action_repeat, int) \
+                            else (action_repeat[1] - action_repeat[0])/2 + action_repeat[0]
+environment = gym.make("Breakout-v0", frameskip=action_repeat)
 input_image_dims = (84, 84)
 num_stack = 4
 output_size = environment.action_space.n  # Same as number of actions
@@ -74,7 +74,7 @@ model.compile(optimizer=optimizer,
 """
 Epochs & Batch Sizes
 
-IMPORTANT: Each iteration corresponds to # `builtin_action_repeat_avg` frames on average (4 in our case)
+IMPORTANT: Each iteration corresponds to # `action_repeat_avg` frames on average (4 in our case)
 Each weight update in turn corresponds to `grad_update_frequency` iterations.
 
 We need these different measures because minibatch updates are counted in weight updates,
@@ -83,7 +83,7 @@ is frames.
 """
 
 grad_update_frequency = 4  # Measured in iterations. From paper, 1 grad update = 4 actions = 16 frames
-final_exploration_iteration = int(10**6 / builtin_action_repeat_avg)  # Measured in frames, converted to iterations
+final_exploration_iteration = int(10**6 / action_repeat_avg)  # Measured in frames, converted to iterations
 train_exploration_schedule = (lambda iteration: max(0.1, 1 - 0.9 * iteration/final_exploration_iteration))
 eval_exploration_schedule = (lambda iteration: 0.05)
 train_batch_size = 32
@@ -91,12 +91,12 @@ target_update_frequency = int(10**4 * grad_update_frequency)  # Measured in weig
 gamma = 0.99
 epoch_length = 50000  # Measured in weight updates, used by generator getter
 epoch_iterations = epoch_length * grad_update_frequency  # Convert epoch length from weight updates to iterations
-total_training_iterations = 200 * 10**6 // builtin_action_repeat_avg  # Measured in frames, converted to iterations
+total_training_iterations = 200 * 10**6 // action_repeat_avg  # Measured in frames, converted to iterations
 num_epochs = int(math.ceil(total_training_iterations / epoch_iterations))
 replay_buffer_size = 10**6  # Note that the paper measures this number in frames
 replay_buffer_min = 50 * 10**3  # Measured in frames STORED IN BUFFER, technically more frames are played due to repeats
 eval_episodes = np.inf  # No upper bound on the number of episodes evaluated
-eval_max_iter = 10000 // builtin_action_repeat_avg  # Upper bound of evaluation measured in frames, converted to iters
+eval_max_iter = 10000 // action_repeat_avg  # Upper bound of evaluation measured in frames, converted to iters
 eval_num_samples = 1000
 take_pair_max = True
 
@@ -115,7 +115,6 @@ train_gen = AtariSequence(model,
                           batch_size=train_batch_size,
                           grad_update_frequency=grad_update_frequency,
                           target_update_frequency=target_update_frequency,
-                          action_repeat=1,  # No need for manual action repeat, Gym environment handles repeats
                           gamma=gamma,
                           epoch_length=epoch_length,
                           replay_buffer_size=replay_buffer_size,
